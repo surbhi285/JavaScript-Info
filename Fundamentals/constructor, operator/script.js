d93014464fa5e8e67058ef21a109a99ad3d7a717
Surbhi <sursingh52@gmail.com>
2023-06-28 21:52:41 +0530

    constructor

---
+++ Fundamentals/constructor, operator/script.js
@@ -0,0 +1,65 @@
+// the regular{..} syntax allows us to create one object but often we need to create simliar objects, 
+// like multiple users or menu items and so on. for this we can use contructor function and new keyword.
+
+//--------------CONSTRUCTOR FUNCTION
+// Technically they are regular function. There are two convention though:
+//1-> they are named with capital letter first
+//2-> they should be excuted only with "new" operator
+
+function User(name){
+    this.name=name;
+    this.isAdmin=false;
+}
+let user = new User("jack");
+
+console.log(user.name); // jack
+console.log(user.admin);// false
+
+// when function is executed with new it does the following step;
+//1-> a new empty object is created and assigned to "this"
+//2-> The function body executes. Usually it modifies "this", adds new properties to it
+//3-> The value of "this" is returned.
+
+/* so new User(.. .) works like this
+
+function User(name){
+    // this ={} (implicitly)
+    // add properties to "this"
+    this.name = name;
+    this.admin = admin;
+
+   // return(this);(implicitly)
+} */
+
+// so we want to creater other users, we can call new User("ann"), new user("alice") and so on.
+
+// NOTE -> so technically any function (except arrows functions, as they dont have "this") can be use as 
+// constructor. It can be run with new, it will execute algorithm above. The capital letter first is common
+//agreement. to make it clear that function is to be run with new.
+
+
+//----------->new function()
+// if we have many lines of code all about creation of single complex object, we can wrap tham in an immediately
+//called constructor function, for eg
+
+let user1 = new function(){ // creating function and immediately call it with new
+    this.name=name;
+    this.isAdmin=false;
+};
+// but this constructor function cant be called again because it is not saved anywhere, just created and called.
+
+///------------>Return from constructor
+/* ususally constructor dont have return statement. Their task is to write all necessary stuff into "this"
+and it will automatically become result.
+so if it is result statement, the the rule is simple
+1-> if return is called with an object then the object is returned instead of "this"
+2-> if return is called with primitive then it is ignored.
+*/
+
+// In below eg return overrides this by returning an object
+function BigUser(){
+    this.name="john";
+    return{name:"ram"}; // <----return this object
+}
+console.log(new BigUser().name); // ram got that object
+
